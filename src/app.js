9d55cc95708c9676236c2714b1b86f1d5d358b16
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-05-21 13:45:18 +0900

    Extract todo reducer from todos reducer

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 import deepFreeze from 'deep-freeze';
 import expect from 'expect';
 
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false
+      };
+    case 'TOGGLE_TODO':
+      if(state.id === action.id) {
+        return Object.assign({}, state, {
+          completed: !state.completed
+        });
+      }
+      return state;
+    default:
+      return state;
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if(todo.id === action.id) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          });
-        }
-        return todo;
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
