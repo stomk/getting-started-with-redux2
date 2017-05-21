c2f683b1efc90c249121ebe6234696abc60c2a00
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-05-21 13:35:03 +0900

    Add test

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import deepFreeze from 'deep-freeze';
+import expect from 'expect';
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
@@ -22,3 +25,28 @@ const todos = (state = [], action) => {
       return state;
   }
 }
+
+const testAddTodo = () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }
+  ];
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
+testAddTodo();
+console.log('All tests passed!');
