a392fd4f6dcceea15e2673ff2e4f2d866250f206
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-05-28 11:08:54 +0900

    Extract AddTodo component

--- src/app.js
+++ src/app.js
@@ -110,6 +110,25 @@ const TodoList = ({
   </ul>
 );
 
+const AddTodo = ({
+  onAddClick
+}) => {
+  let input;
+  return (
+    <div>
+      <input ref={node => {
+        input = node;
+      }} />
+      <button onClick={() => {
+        onAddClick(input.value);
+        input.value = '';
+      }}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
 const getVisibleTodos = (
   todos,
   filter
@@ -143,19 +162,15 @@ class TodoApp extends React.Component {
     );
     return (
       <div>
-        <input ref={node => {
-          this.input = node;
-        }} />
-        <button onClick={() => {
-          store.dispatch({
-            type: 'ADD_TODO',
-            text: this.input.value,
-            id: nextTodoId++
-          });
-          this.input.value = '';
-        }}>
-          Add Todo
-        </button>
+        <AddTodo
+          onAddClick={text =>
+            store.dispatch({
+              type: 'ADD_TODO',
+              id: nextTodoId++
+              text: text,
+            })
+          }
+        />
         <TodoList
           todos={visibleTodos}
           onTodoClick={id =>
